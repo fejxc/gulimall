af6b3cefae5793dbfef4ee7aa8b2d19d4ed40622
孙昀 <1031364436@qq.com>
2022-07-21 20:59:42 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/suny/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suny.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
