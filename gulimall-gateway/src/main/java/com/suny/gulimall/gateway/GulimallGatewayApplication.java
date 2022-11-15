b15647324375306f730a49b8950f822c450d8810
fejxc <1031364436@qq.com>
2022-11-15 10:17:07 +0800

    提交最新pom文件

---
+++ gulimall-gateway/src/main/java/com/suny/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.suny.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 排除数据库启动的依赖
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
---
+++ gulimall-gateway/src/test/java/com/suny/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.suny.gulimall.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
